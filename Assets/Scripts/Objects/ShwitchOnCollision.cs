a6ff0c1e1672af36c8597ec38baff8bad06c9226
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-08-29 17:09:09 +0200

    [Programowanie] Przycisk na strzał

---
+++ Assets/Scripts/Objects/ShwitchOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ShwitchOnCollision : MonoBehaviour
+{
+    [SerializeField] private bool killSelf;
+    [SerializeField] private string collisonTag;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == collisonTag)
+        {
+            collision.GetComponent<IInteractable>().Interact();
+            if(killSelf)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
