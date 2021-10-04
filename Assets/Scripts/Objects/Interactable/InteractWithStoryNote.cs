c01fe870a2c505f7b0e7fa1b10ccb61b463bd204
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-10-04 12:08:05 +0200

    [Programowanie] Ustawienie Animacji NotePada

--- Assets/Scripts/Objects/Interactable/InteractWithStoryNote.cs
+++ Assets/Scripts/Objects/Interactable/InteractWithStoryNote.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class InteractWithStoryNote : InteractableObject, IInteractable
 {
     [SerializeField] StoryTextNote storyNote;
+    private Animator animator;
 
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
     public override void Interact()
     {
+        animator.SetTrigger("activate");
         EventBroker.CallUpdateStoryText(storyNote.StoryIndex.ToString(), storyNote.StoryHeader, storyNote.StoryText);
         EventBroker.CallSwithcOnOffStoryPanel();
     }
